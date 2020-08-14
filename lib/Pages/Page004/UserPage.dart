0877e6dd3d95dc53715a7f43700ac4216fba8d47
Edwin Shiu <edwin2000412@gmail.com>
2020-08-14 23:57:24 +0800

    UserPage

--- lib/Pages/Page004/UserPage.dart
+++ lib/Pages/Page004/UserPage.dart
@@ -31,82 +31,132 @@ class AnonPageState extends State<AnonPage> {
             ),
           ),
           Expanded(
-          child: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 100),
             child: Column(
               children: [
-                Container(
-                  color: Colors.yellow,
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      Align(
-                        alignment: Alignment.center,
-                        child: Icon(
-                          Icons.account_circle,
-                          size: 250,
+                Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: Icon(
+                        Icons.account_circle,
+                        size: 250,
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                      ),
+                      child: Text(
+                        "Sonnion: 3000",
+                        style: GoogleFonts.ptSans(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.w500,
+                          color: sonyBlack,
                         ),
                       ),
-                      Container(
-                        color: Colors.red,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Ink(
+                  width: double.maxFinite,
+                  color: sonyBlack,
+                  child: InkWell(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
                         child: Text(
-                          "Sonnion",
+                          "Account Information",
                           style: GoogleFonts.ptSans(
                             fontSize: 20.0,
                             fontWeight: FontWeight.w500,
-                            color: sonyBlack,
+                            color: Colors.white,
                           ),
                         ),
                       ),
-                    ],
-                  ),   
-                ),
-                Material(
-                  color: sonyBlack,
-                  child: InkWell(
-                    child: Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                      "Account Information",
-                      style: GoogleFonts.ptSans(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.white,
-                      ),
-                    ),
                     ),
                     onTap: () {
                       print("Account Information");
                     },
                   ),
                 ),
-                RaisedButton(
-                  child: Text(
-                    "Warranty",
-                    style: GoogleFonts.ptSans(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.white,
-                    ),
-                  ),
+                SizedBox(height: 20),
+                Ink(
+                  width: double.maxFinite,
                   color: sonyBlack,
-                  onPressed: () {
-                    print("Warranty");
-                  }, 
+                  child: InkWell(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          "Product Warranty",
+                          style: GoogleFonts.ptSans(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    onTap: () {
+                      print("Warranty");
+                    },
+                  ),
                 ),
-                RaisedButton(
-                  child: Text(
-                    "Log Out",
-                    style: GoogleFonts.ptSans(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.white,
-                    ),
-                  ),
+                SizedBox(height: 20),
+                Ink(
+                  width: double.maxFinite,
                   color: sonyBlack,
-                  onPressed: () async {
-                    await _auth.signOut();
-                  }
-                )
+                  child: InkWell(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          "Order Record",
+                          style: GoogleFonts.ptSans(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    onTap: () {
+                      print("Order");
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                Ink(
+                  width: double.maxFinite,
+                  color: sonyBlack,
+                  child: InkWell(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          "Log Out",
+                          style: GoogleFonts.ptSans(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    onTap: () async {
+                      print("Log Out");
+                      await _auth.signOut();
+                    },
+                  ),
+                ),
               ],
             )
           ),
